282fb1b19a5ca485d59198e2252b6671325a2b51
DuFong <youngjewall@me.com>
2022-01-14 00:50:48 +0900

    Add gradle

---
+++ src/main/java/com/wejbson/mockInvestment/MockInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.wejbson.mockInvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockInvestmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockInvestmentApplication.class, args);
+    }
+
+}
