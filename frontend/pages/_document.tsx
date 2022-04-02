18c911530a3b2c0a94a9eb18d0d957f52a3db351
Hyeongyu Kim <dhk0561@naver.com>
2022-04-03 01:42:06 +0900

    build: PWA 빌드 적용

---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class CustomDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+                    <meta name="description" content="Description" />
+                    <meta name="keywords" content="Keywords" />
+                    <title>Next.js PWA Example</title>
+
+                    <link rel="manifest" href="./manifest.json" />
+                    <link href="./icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+                    <link href="./icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+                    <link rel="apple-touch-icon" href="./icons/apple-touch-icon-180x180.png" />
+                    <meta name="theme-color" content="#FFFFFF" />
+                </Head>
+                <body>
+                    <Main />
+                </body>
+                <NextScript />
+            </Html>
+        );
+    }
+}
