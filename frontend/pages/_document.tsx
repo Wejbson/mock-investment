a386850cd63a59a89c3bf761a09659aaac985a54
Hyeongyu Kim <dhk0561@naver.com>
2022-04-05 22:34:53 +0900

    fix: 빌드 오류 수정

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -6,14 +6,14 @@ export default class CustomDocument extends Document {
             <Html>
                 <Head>
                     <meta charSet="utf-8" />
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                     <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
                     <meta name="description" content="Description" />
                     <meta name="keywords" content="Keywords" />
                     <title>Next.js PWA Example</title>
 
                     <link rel="manifest" href="./manifest.json" />
-                    <link href="./icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+                    <link href="./icons/favicon-16x16.png" rel="icon" type="image/png" ㅔsizes="16x16" />
                     <link href="./icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
                     <link rel="apple-touch-icon" href="./icons/apple-touch-icon-180x180.png" />
                     <meta name="theme-color" content="#FFFFFF" />
