4db832e13758f0429641fce1ee81b4e42bd1ec91
DuFong <youngjewall@me.com>
2022-04-05 23:05:42 +0900

    feat : Account 서비스 인터페이스 및 구현체 생성 feat : Member 객체 및 회원가입 Requst DTO 생성 feat : 테스트 API 추가 (POST : "/echo")

---
+++ src/main/java/com/wejbson/mockInvestment/domain/member/domain/Member.java
@@ -0,0 +1,12 @@
+package com.wejbson.mockInvestment.domain.member.domain;
+
+public class Member {
+
+    private String id;
+
+    private String password;
+
+    private String name;
+
+    private String tel;
+}
